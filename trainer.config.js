d1cfb77c7d7883d74eabac623ec3d0b61d328b6d
dannyromans13 <dannyromans13@gmail.com>
2026-03-27 22:22:51 -0600

    Agregadas las características de trainer.config.js

---
+++ trainer.config.js
@@ -0,0 +1,21 @@
+const TRAINER = {
+  // ── Quién eres ──
+  name: "DANNY",
+  hometown: "San José, Costa Rica",
+  catchphrase: "El voltaje siempre gana.",
+
+  // ── Nombre del PokeAPI o ID ──
+  favoritePokemon: "pikachu",
+  nickname: "Pik",
+  battleCry: "¡Piiiiiikachuuuu!",
+
+  // ── Movimiento Definitivo ──
+  definitiveMoveName: "Tormenta Eléctrificante",
+  definitiveMoveFlavor: "Cuando el cielo se vuelve de Pikachu no tienes escape.",
+
+  // ── Mensajes Finales ──
+  winMessage: "Para eso entrenamos duro!.",
+  loseMessage: "Volveremos más fuertes!.",
+};
+
+export default TRAINER;
